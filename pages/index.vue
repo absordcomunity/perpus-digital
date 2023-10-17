f364876e3df0fdba4d2344b3df8e1de5e627d7fd
fbrianiacaa <113565624+tasyafbriani@users.noreply.github.com>
2023-10-17 09:49:22 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>pengunjung</h1>
+        <NuxtLink to="/tambah">ISI BUKU</NuxtLink>
+        <table border="1" width="50%"></table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>id</th>
+                <th>tanggal</th>
+                <th>waktu</th>
+                <th>nama</th>
+                <th>anggota</th>
+                <th>keperluan</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr v-for="visitor in visitors" :key="visitor.id">
+                    <td>{{ visitor.id }}</td>
+                    <td>{{ visitor.tanggal }}</td>
+                    <td>{{ visitor.waktu }}</td>
+                    <td>{{ visitor.nama }}</td>
+                    <td>{{ visitor.anggota }}</td>
+                    <td>{{ visitor.keperluan }}</td>
+                </tr>
+            </tbody>
+    </div>
+</template>
+
+<script setup>
+const supabase =useSupabaseClient()
+const visitors =ref([])
+
+async function getData() {
+    const {data,error} =await supabase
+    .from ("pengunjung")
+    .select()
+    visitors.value = data
+}
+onMounted( () => {
+    getData()
+})
+</script>
---
+++ pages/tambah.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Isi Buku Tamu</h1>
+        <form @submit.prevent="simpan()">
+            <input v-model="id" placeholder="id"> <br />
+            <input v-model="tanggal" placeholder="tanggal"> <br />
+            <input v-model="waktu" placeholder="waktu"> <br />
+            <input v-model="nama" placeholder="nama"> <br />
+            <input v-model="anggota" placeholder="anggota"> <br />
+            <input v-model="keperluan" placeholder="keperluan"> <br />
+        </form>
+    </div>
+    </template>
+    
+    <script setup>
+    const supabase = useSupabaseClient()
+    const id = ref()
+    const tanggal = ref()
+    const waktu = ref()
+    const nama = ref()
+    const anggota = ref()
+    const keperluan = ref()
+    
+    async function simpan() {
+        await supabase
+        .form("pengunjung")
+        .insert({
+            id: id.value,
+            tanggal: tanggal.value,
+            waktu: waktu.value,
+            nama: nama.value,
+            anggota: anggota.value,
+            keperluan: keperluan.value,
+        })
+        navigateTo("/")
+    }
+    </script>
+    
+    <style>
+    
+    </style>
