a117dd5994ded7b57ae79b414f03ff2a30b83692
fbrianiacaa <113565624+tasyafbriani@users.noreply.github.com>
2023-10-18 09:48:16 +0700

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,39 @@
 <template>
     <div>
-        <h1>pengunjung</h1>
-        <NuxtLink to="/tambah">ISI PENGUNJUNG</NuxtLink>
-        <table border="1" width="50%"></table>
-            <tbody>
-                <tr v-for="visitor in visitors" :key="visitor.id">
-                    <td>{{ visitor.id }}</td>
-                    <td>{{ visitor.tanggal }}</td>
-                    <td>{{ visitor.waktu }}</td>
-                    <td>{{ visitor.nama }}</td>
-                    <td>{{ visitor.anggota }}</td>
-                    <td>{{ visitor.keperluan }}</td>
-                </tr>
-            </tbody>
+        <NuxtLink to="/tambah">Isi Buku Tamu</NuxtLink>
+        <table border="1" width="50%">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>TANGGAL dan WAKTU</th>
+                <th>NAMA</th>
+                <th>ANGGOTA</th>
+                <th>KEPERLUAN</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="pengunjung in visitors" :key="pengunjung.id">
+                <td>{{ pengunjung.id }}</td>
+                <td>{{ pengunjung.tanggal }}</td>
+                <td>{{ pengunjung.nama }}</td>
+                <td>{{ pengunjung.anggota }}</td>
+                <td>{{ pengunjung.keperluan }}</td>
+            </tr>
+        </tbody>
+        </table>
     </div>
 </template>
 
 <script setup>
-const supabase =useSupabaseClient()
-const visitors =ref([])
+const client = useSupabaseClient()
+const visitors = ref([])
 
 async function getData() {
-    const {data,error} =await supabase
-    .from ("pengunjung")
-    .select()
-    visitors.value = data
+    const {data,error} =await client
+    .from ('pengunjung')
+    .select('*')
+    if(data) visitors.value = data
 }
-onMounted( () => {
-    getData()
-})
+onMounted(() => getData())
+
 </script>
--- pages/tambah.vue
+++ pages/tambah.vue
@@ -1,42 +1,36 @@
 <template>
     <div>
-        <h1>Isi Buku Tamu</h1>
-        <form @submit.prevent="simpan()">
-            <input v-model="id" placeholder="id"> <br />
-            <input v-model="tanggal" placeholder="tanggal"> <br />
-            <input v-model="waktu" placeholder="waktu"> <br />
-            <input v-model="nama" placeholder="nama"> <br />
-            <input v-model="anggota" placeholder="anggota"> <br />
-            <input v-model="keperluan" placeholder="keperluan"> <br />
-            <button type="submit">kirim</button>
+        <h2>Isi Buku Tamu</h2>
+        <form>
+            <input v-model="form.nama" type="text" placeholder="Nama ..." required> <br >
+            <select v-model="form.anggota">
+                <option value="">Pilih Anggota</option>
+                <option v-for="a in anggota" :key="a.id" value="a.id">{{ a.nama }}</option>
+            </select> <br>
+            <textarea v-model="form.keperluan" cols="30" rows="10" placeholder="Keperluan..."></textarea> <br>
+            <button type=“submit”>Kirim</button> 
+            <NuxtLink to=“/“>Kembali</NuxtLink>
         </form>
     </div>
     </template>
     
     <script setup>
     const supabase = useSupabaseClient()
-    const id = ref()
-    const tanggal = ref()
-    const waktu = ref()
-    const nama = ref()
-    const anggota = ref()
-    const keperluan = ref()
-    
-    async function simpan() {
-        await supabase
-        .form("pengunjung")
-        .insert({
-            id: id.value,
-            tanggal: tanggal.value,
-            waktu: waktu.value,
-            nama: nama.value,
-            anggota: anggota.value,
-            keperluan: keperluan.value,
-        })
-        navigateTo("/")
+    const anggota= ref([])
+    const form = reactive({
+        nama: "",
+        anggota: "",
+        keperluan: "",
+
+    })
+
+    async function getAnggota() {
+        const {data,error} = await supabase.from('anggota').select()
+        if(data) anggota.value=data
     }
+    async function SimpanKunjungan() {
+        const { error } = await supabase.from('pengunjung').insert(from)
+    }
+    onMounted(() => getAnggota())
     </script>
     
-    <style>
-    
-    </style>
