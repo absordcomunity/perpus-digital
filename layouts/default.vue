92b82267e27d9b71d494a2c0e5fa7989d0787eea
fbrianiacaa <113565624+tasyafbriani@users.noreply.github.com>
2023-10-18 09:54:56 +0700

    Add files via upload

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>catatan pengunjung perpustakaan</h1>
+        <h1><center>Catatan Pengunjung Perpustakaan</center></h1>
         <hr>
         <slot />
     </div>  
